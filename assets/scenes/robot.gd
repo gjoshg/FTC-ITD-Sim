7e1ab8b7f06f1d789c99526da74f7149b9e461e2
Josh G <jogr1350@gmail.com>
2024-12-30 19:58:50 -0500

    Update robot.gd
    add odometry-inspired move and rotate in one command functionality

--- assets/scenes/robot.gd
+++ assets/scenes/robot.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 
-enum COMMANDS {MOVEMENT, ROTATION, WAIT, ARM_MOVEMENT, ARM_ROTATION, CLAW_TOGGLE}
+enum COMMANDS {MOVEMENT, ROTATION, MOVE_AND_ROTATE, WAIT, ARM_MOVEMENT, ARM_ROTATION, CLAW_TOGGLE}
 enum CLAW_STATE{OPEN, CLOSE}
 var robotActions = []
 var movementSpeed = 100
@@ -57,31 +57,92 @@ func blueLeft():
 	setLiftTargetPosition(0)
 	turn(180)
 	moveForward(10)
-	setClawState(CLAW_STATE.OPEN)
+	setClawState(CLAW_STATE.OPEN) 
 	turn(30)
 	moveForward(350)
 	
 
 func blueRight():
-	moveRight(150)
-	moveForward(200)
-	moveRight(50)
-	moveForward(-200)
-	moveForward(200)
-	moveRight(30)
-	moveForward(-200)
-	moveForward(200)
-	moveRight(30)
-	moveForward(-200)
+	moveToPositionWithHeading(5, 5, 180)
+	moveRight(208)
+	moveForward(80)
+	setClawState(CLAW_STATE.OPEN)
+	setClawState(CLAW_STATE.CLOSE)
+	turn(-180)
+	moveForward(29)
+	setClawState(CLAW_STATE.OPEN)
+	moveBackward(15)
+	moveRight(10)
+	wait(2)
+	moveForward(15)
+	setClawState(CLAW_STATE.CLOSE)
+	moveRight(200)
+	turn(0)
+	moveForward(40)
+	setClawState(CLAW_STATE.OPEN)
+	moveBackward(10)
+	moveRight(230)
+	moveForward(10)
+	setClawState(CLAW_STATE.CLOSE)
+	turn(-180)
+	moveForward(40)
+	setClawState(CLAW_STATE.OPEN)
+	moveBackward(15)
+	moveRight(35)
+	wait(2)
+	moveForward(15)
+	setClawState(CLAW_STATE.CLOSE)
+	moveRight(200)
+	turn(0)
+	moveForward(40)
+	setClawState(CLAW_STATE.OPEN)
+	moveBackward(10)
+	moveRight(200)
+	moveForward(130)
+	moveRight(53)
+	moveBackward(150)
+	moveForward(20)
+	turn(-180)
+	moveRight(49)
+	wait(2)
+	moveForward(20)
+	setClawState(CLAW_STATE.CLOSE)
+	moveRight(170)
+	turn(0)
 	moveForward(50)
-	wait(3)
-	moveForward(-50)
+	setClawState(CLAW_STATE.OPEN)
+	moveBackward(40)
+	turn(-180)
+	moveLeft(180)
+	moveForward(30)
+	
+pass
 
 func redLeft():
 	pass
 
 func redRight():
 	pass
+
+"""
+		Robot Functionality:
+			- moveForward(units) 	| move robot forwards
+			- moveBackward(units)	| move robot backwards
+			- moveLeft(units)		| move robot left
+			- moveRight(units)		| move robot right
+			- moveToPointWithHeading(# horizontal tiles, # vertical tiles, degrees)
+									| move over x tiles, move down y tiles, and rotate n degrees
+			- setClawState(CLAW_STATE.OPEN or CLAW_STATE.CLOSE)
+									| OPENs or CLOSEs robot claw
+			
+			- setLiftTargetPosition(units)
+									| Sets arm length 
+			- wait (seconds)		| Makes robot do nothing
+			- turn (degrees)		| Turns to heading
+"""
+
+func moveToPositionWithHeading(x, y, degrees):
+	robotActions.push_back({type = COMMANDS.MOVE_AND_ROTATE, value = {x = (100*x-300), y =  100*y-300, degrees = degrees - 360 if degrees > 180 else degrees}, distanceToTravel = -1})
 
 func setClawState(state: CLAW_STATE):
 	robotActions.push_back({type = COMMANDS.CLAW_TOGGLE, value = state})
@@ -163,17 +224,41 @@ func _physics_process(delta: float) -> void:
 					childSample.reparent(claw)
 					childSample.position = claw.position - Vector2(0, 20) # lucky number 20
 			robotActions.pop_front()
-			
+		elif activeCommand.type == COMMANDS.MOVE_AND_ROTATE:
+			var targetX = activeCommand.value.x
+			var targetY = activeCommand.value.y
+			var targetRotation = activeCommand.value.degrees
+			if activeCommand.distanceToTravel == -1:
+				activeCommand.distanceTraveled = 0.0
+				activeCommand.distanceToTravel = Vector2(targetX, targetY).distance_to(position)
+			# checking if x, y, and rotation are at targeted values or if it has traveled "enough" distance
+			if (isInBounds(position.x, targetX, positionalEpsilon) and isInBounds(position.y, targetY, positionalEpsilon) or (activeCommand.distanceTraveled + positionalEpsilon >= activeCommand.distanceToTravel)) and isInBounds(rotation_degrees, targetRotation, rotationalEpsilon):
+				robotActions.pop_front()
+			else:
+				# wrote with the help of Mr. Dereck
+				if activeCommand.distanceTraveled + positionalEpsilon <= activeCommand.distanceToTravel:
+					var newPosition = position.move_toward(Vector2(targetX, targetY), movementSpeed * delta)
+					activeCommand.distanceTraveled += newPosition.distance_to(position)
+					position = newPosition
+
+				if not isInBounds(rotation_degrees, targetRotation, rotationalEpsilon):
+					if rotation_degrees < targetRotation:
+						rotation_degrees += rotationSpeed * delta
+					else:
+						rotation_degrees -= rotationSpeed * delta	
+		
 					
 	# needed to push rigidbodies				
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
 			c.get_collider().apply_central_impulse(-c.get_normal() * robotWeight)
-	
 
 	move_and_slide()
 
+
+func isInBounds(val1, val2, epsilon):
+	return val1 - epsilon <= val2 and val2 <= val1 + epsilon
 
 func _on_arm_body_entered(body: Node) -> void:
 	pass
